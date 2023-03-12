57aeb9e451d1c07d452ff83952b9c877a721680a
LAPTOP-G9CBJI3P\tantrungse <tantrungse@gmail.com>
2023-03-12 16:07:58 +0700

    Add status to dining review entity

--- src/main/java/com/example/diningreviewapi/model/Review.java
+++ src/main/java/com/example/diningreviewapi/model/Review.java
@@ -1,8 +1,6 @@
 package com.example.diningreviewapi.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "dining_review")
+@Table(name = "review")
 public class Review {
     @Column(nullable = false, unique = true)
     private String displayName;
@@ -32,4 +30,7 @@ public class Review {
 
     @Column
     private String commentary;
+
+    @Enumerated(EnumType.STRING)
+    private ReviewStatus status;
 }
---
+++ src/main/java/com/example/diningreviewapi/model/ReviewStatus.java
@@ -0,0 +1,7 @@
+package com.example.diningreviewapi.model;
+
+public enum ReviewStatus {
+    PENDING,
+    ACCEPTED,
+    REJECTED
+}
