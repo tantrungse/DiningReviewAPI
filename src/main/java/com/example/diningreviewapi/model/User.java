e6e4e6ed1a0b628be3c1a3eef1e26e8e4aaad93f
LAPTOP-G9CBJI3P\tantrungse <tantrungse@gmail.com>
2023-03-12 15:42:36 +0700

    Define the user entity

--- src/main/java/com/example/diningreviewapi/model/User.java
+++ src/main/java/com/example/diningreviewapi/model/User.java
@@ -1,2 +1,40 @@
-package com.example.diningreviewapi.model;public class User {
+package com.example.diningreviewapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String displayName;
+
+    @Column
+    private String city;
+
+    @Column
+    private String state;
+
+    @Column
+    private String zipcode;
+
+    @Column
+    private boolean isPeanutAllergy;
+
+    @Column
+    private boolean isEggAllergy;
+
+    @Column
+    private boolean isDairyAllergy;
 }
